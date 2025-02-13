c47c3e6ccb40b1c11c0b8fcaeaff483614a22276
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-13 18:10:30 -0500

    feat: have Subscriber expect a `RedisKey` instance
    Technically a breaking change that should’ve been done before v0.4.0 if I had tested this earlier.

--- src/subscriber.ts
+++ src/subscriber.ts
@@ -1,11 +1,13 @@
+import { TAnySchema, TSchema } from "@sinclair/typebox";
 import { RedisClient } from "./client";
 import { createRedis } from "./create-redis";
+import { RedisKey, Value } from "./key";
 import { RedisClientOptions } from "./type";
 
 type RedisInstance = ReturnType<typeof createRedis>;
 
-interface MessageHandler {
-  (message: string, channel: string): void;
+interface MessageHandler<T extends TSchema = TAnySchema> {
+  (message: Value<T>, channel: string): void;
 }
 
 export class Subscriber {
@@ -36,31 +38,34 @@ export class Subscriber {
     await this.client.close();
   }
 
-  async subscribe(
-    channel: string,
-    handler: MessageHandler,
+  async subscribe<T extends TSchema>(
+    channel: RedisKey<T>,
+    handler: MessageHandler<T>,
   ): Promise<() => void> {
-    let handlers = this.subscriptions.get(channel);
+    let handlers = this.subscriptions.get(channel.text);
     if (!handlers) {
       handlers = new Set();
-      this.subscriptions.set(channel, handlers);
-      await this.client.sendRaw("SUBSCRIBE", channel);
+      this.subscriptions.set(channel.text, handlers);
+      await this.client.sendRaw("SUBSCRIBE", channel.text);
     }
     handlers.add(handler);
     return () => this.unsubscribe(channel, handler);
   }
 
-  async unsubscribe(channel: string, handler: MessageHandler): Promise<void> {
-    const handlers = this.subscriptions.get(channel);
+  async unsubscribe<T extends TSchema>(
+    channel: RedisKey<T>,
+    handler: MessageHandler<T>,
+  ): Promise<void> {
+    const handlers = this.subscriptions.get(channel.text);
     if (handlers?.has(handler)) {
       handlers.delete(handler);
       if (handlers.size === 0) {
-        this.subscriptions.delete(channel);
+        this.subscriptions.delete(channel.text);
 
         if (this.subscriptions.size === 0) {
           await this.disconnect();
         } else {
-          await this.client.sendRaw("UNSUBSCRIBE", channel);
+          await this.client.sendRaw("UNSUBSCRIBE", channel.text);
         }
       }
     }
