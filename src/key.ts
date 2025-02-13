b1a36fe4a5fa280ea29ff9b86f3c775775d7e5b4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-13 17:39:59 -0500

    feat: allow numbers in `RedisKey#join`

--- src/key.ts
+++ src/key.ts
@@ -42,7 +42,7 @@ export class RedisKey<
    * Derive a new key by prefixing the current key with the given keys. When multiple keys are
    * passed in, they will be joined with a colon.
    */
-  join(...keys: [string, ...string[]]) {
+  join(...keys: [string | number, ...(string | number)[]]) {
     return new RedisKey(`${this.text}:${keys.join(":")}`, this.schema);
   }
 
